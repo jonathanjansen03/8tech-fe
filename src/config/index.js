8915121d08e95c2657b4a38cc66d0624628e2a5f
Jonathan Jansen Subroto <jonathan.subroto@gdn-commerce.com>
2023-08-08 00:38:05 +0700

    refactor router

---
+++ src/config/index.js
@@ -0,0 +1,6 @@
+export default {
+  pages: {
+    home: '/',
+    about: '/about'
+  },
+};
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,23 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import config from '@/config';
+
+const HomePage = () => import('@/pages/HomePage.vue');
+const AboutPage = () => import('@/pages/AboutPage.vue');
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: config.pages.home,
       name: 'home',
-      component: HomeView
+      component: HomePage,
     },
     {
-      path: '/about',
+      path: config.pages.about,
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      component: AboutPage,
+    },
+  ],
+});
 
-export default router
+export default router;
