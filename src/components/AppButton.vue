1054706e7c901e9c8c042e3f0617ec317cd16d46
Jonathan Jansen Subroto <jonathan.subroto@gdn-commerce.com>
2023-11-05 19:45:18 +0700

    fix button style

--- src/components/AppButton.vue
+++ src/components/AppButton.vue
@@ -6,12 +6,16 @@ const props = defineProps({
 
 <template>
   <button
-    class="border-2 border-blue-800 px-5 py-1 rounded-xl duration-500 linear"
+    class="px-5 py-2 rounded-xl transition focus:outline-none"
     :class="{
       'bg-blue-800': !outline,
+      'text-white': !outline,
+      'hover:bg-blue-900': !outline,
+      'active:bg-blue-950': !outline,
       'bg-white': outline,
       'text-blue-800': outline,
-      'text-white': !outline,
+      'hover:bg-blue-200': outline,
+      'active:bg-blue-300': outline,
     }"
   >
     <slot></slot>
