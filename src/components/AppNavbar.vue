79fbbe5de22d471f13ec02ec985c816dc99fae45
Jonathan Jansen Subroto <jonathan.subroto@gdn-commerce.com>
2023-11-05 19:45:09 +0700

    add non mobile navbar

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -20,33 +20,70 @@ const toggleNavbar = () => {
     class="navbar sticky -top-1 w-full drop-shadow-[0_4px_5px_rgba(0,0,0,0.5)] z-10"
   >
     <!-- mobile navbar -->
-    <div class="bg-zinc-800 flex items-center justify-between px-5 py-5">
-      <img
-        :src="logo"
-        alt="Logo"
-        class="cursor-pointer h-auto w-36"
-        @click="router.push(config.pages.home)"
-      />
-      <Bars3Icon
-        v-show="!isOpen"
-        @click="toggleNavbar"
-        class="cursor-pointer w-10"
-      />
-      <XMarkIcon
-        v-show="isOpen"
-        @click="toggleNavbar"
-        class="cursor-pointer w-10"
-      />
+    <div class="mobile-navbar md:hidden">
+      <div class="bg-zinc-800 flex items-center justify-between px-5 py-5">
+        <img
+          :src="logo"
+          alt="Logo"
+          class="cursor-pointer h-auto w-36"
+          @click="router.push(config.pages.home)"
+        />
+        <Bars3Icon
+          v-show="!isOpen"
+          @click="toggleNavbar"
+          class="cursor-pointer w-10"
+        />
+        <XMarkIcon
+          v-show="isOpen"
+          @click="toggleNavbar"
+          class="cursor-pointer w-10"
+        />
+      </div>
+      <div class="duration-500 overflow-x-clip relative">
+        <div
+          class="navbar__links absolute bg-zinc-900 flex flex-col gap-8 min-h-screen items-end px-5 py-5 w-52 duration-500 z-10"
+          :class="isOpen ? 'right-0' : '-right-52'"
+        >
+          <RouterLink to="/" class="mt-5">Home</RouterLink>
+          <RouterLink to="/">Cari Pekerjaan</RouterLink>
+          <RouterLink to="/about">Tentang Kami</RouterLink>
+          <AppButton>Masuk</AppButton>
+        </div>
+      </div>
     </div>
-    <div class="duration-500 overflow-x-clip relative">
-      <div
-        class="navbar__links absolute bg-zinc-900 flex flex-col gap-8 min-h-screen items-end px-5 py-5 w-52 duration-500 z-10"
-        :class="isOpen ? 'right-0' : '-right-52'"
-      >
-        <RouterLink to="/" class="mt-5">Home</RouterLink>
-        <RouterLink to="/">Cari Pekerjaan</RouterLink>
-        <RouterLink to="/about">Tentang Kami</RouterLink>
-        <AppButton>Daftar</AppButton>
+
+    <!-- non-mobile navbar -->
+    <div class="non-mobile-navbar max-md:hidden">
+      <div class="bg-zinc-800 flex items-center px-5 py-5">
+        <img
+          :src="logo"
+          alt="Logo"
+          class="cursor-pointer h-auto w-36"
+          @click="router.push(config.pages.home)"
+        />
+        <div
+          class="navbar__links flex gap-x-16 lg:gap-x-40 items-center justify-center w-full"
+        >
+          <RouterLink
+            to="/"
+            class="text-lg transition duration-300 hover:text-blue-600 after:absolute after:content-[''] after:w-0 after:h-[1px] after:block after:transition-all after:duration-300 after:bottom-1/4 hover:after:w-[3.3rem] hover:after:h-[1px] hover:after:bg-blue-600"
+          >
+            Home
+          </RouterLink>
+          <RouterLink
+            to="/"
+            class="text-lg transition duration-300 hover:text-blue-600 after:absolute after:content-[''] after:w-0 after:h-[1px] after:block after:transition-all after:duration-300 after:bottom-1/4 hover:after:w-[7.75rem] hover:after:h-[1px] hover:after:bg-blue-600"
+          >
+            Cari Pekerjaan
+          </RouterLink>
+          <RouterLink
+            to="/about"
+            class="text-lg transition duration-300 hover:text-blue-600 after:absolute after:content-[''] after:w-0 after:h-[1px] after:block after:transition-all after:duration-300 after:bottom-1/4 hover:after:w-[7.35rem] hover:after:h-[1px] hover:after:bg-blue-600"
+          >
+            Tentang Kami
+          </RouterLink>
+        </div>
+        <AppButton>Masuk</AppButton>
       </div>
     </div>
   </nav>
--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -7,20 +7,6 @@ const isMobile = computed(() => window.innerWidth < 768);
 </script>
 
 <template>
-  <!-- <div v-if="!isMobile" class="flex relative rounded-2xl w-full">
-    <div class="w-full">
-      <MagnifyingGlassIcon
-        class="search-bar__job-name absolute left-3 text-gray-800 top-4 w-5"
-      />
-      <input
-        type="text"
-        class="flex-1 px-10 py-3 rounded-2xl w-full"
-        placeholder="Cari pekerjaan"
-      />
-    </div>
-    <AppButton class="absolute right-1.5 top-1.5">Cari</AppButton>
-  </div> -->
-
   <div
     class="flex flex-col items-center mt-5 relative text-center w-full md:flex-row"
   >
@@ -35,7 +21,7 @@ const isMobile = computed(() => window.innerWidth < 768);
       />
     </div>
     <AppButton
-      class="max-md:mt-2 max-md:w-32 max-md:py-2 md:absolute md:right-1.5 md:top-1.5"
+      class="max-md:mt-2 max-md:w-32 max-md:py-2 md:absolute md:right-1.5 md:top-1.3"
     >
       Cari
     </AppButton>
