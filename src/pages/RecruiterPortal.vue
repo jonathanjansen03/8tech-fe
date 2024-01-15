e668226a16736712e62322ad6310132fe292febe
richard483 <richard.william483@gmail.com>
2024-01-16 01:04:34 +0700

    added table on portal

--- src/pages/RecruiterPortal.vue
+++ src/pages/RecruiterPortal.vue
@@ -1,6 +1,4 @@
 <script setup>
-import SearchBar from '@/components/SearchBar.vue';
-import JobCard from '@/components/JobCard.vue';
 import SideBar from '@/components/SideBar.vue';
 import { onBeforeUnmount, onMounted } from 'vue';
 import { useMainStore } from '@/stores/main.js';
@@ -25,36 +23,37 @@ onBeforeUnmount(() => {
       <h1 class="text-center font-bold mb-8 text-white text-2xl">
         Ketika Tech Talent Bertemu dengan Peluang Tak Terbatas.
       </h1>
-      <SearchBar class="mt-5" />
-      <p class="text-xl mt-10 text-white">Lowongan kerja terbaru:</p>
-      <div
-        class="home__job-cards md:grid md:grid-cols-2 md:gap-x-8 xl:grid-cols-3 2xl:grid-cols-4"
-      >
-        <JobCard
-          :job="{
-            title: 'Software Engineer',
-            company: 'Google',
-            logo: 'https://banner2.cleanpng.com/20180324/iww/kisspng-google-logo-g-suite-google-5ab6f1cee66464.5739288415219388949437.jpg',
-            location: 'California, United States',
-          }"
-        />
-        <JobCard
-          :job="{
-            title: 'UI/UX Designer',
-            company: 'Spotify',
-            logo: 'https://i.pinimg.com/originals/1d/f4/6e/1df46e5b59ceaf54b63302e95644fd80.png',
-            location: 'Stockholm, Sweden',
-          }"
-        />
+      <p class="text-xl mt-10 text-white">Lowongan pekerjaan kamu:</p>
 
-        <JobCard
-          :job="{
-            title: 'Senior Data Scientist',
-            company: 'Amazon',
-            logo: 'https://i.pinimg.com/originals/01/ca/da/01cada77a0a7d326d85b7969fe26a728.jpg',
-            location: 'Washington, United States',
-          }"
-        />
+      <div class="relative overflow-x-auto">
+        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <tr>
+            <th class="px-6 py-3 w-20">
+              No.
+            </th>
+            <th class="px-6 py-3">
+              Title
+            </th>
+            <th class="">
+              Color
+            </th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            <td class="px-6 py-4">
+              1.
+            </td>
+            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+              Apple MacBook Pro 17"
+            </th>
+            <td class="px-6 py-4">
+              Silver
+            </td>
+          </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   </div>
